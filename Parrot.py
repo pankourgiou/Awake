a7df4c7713281b04f1f8de171958959ff6273109
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-12-16 08:26:04 +0200

    Update Parrot.py

--- Parrot.py
+++ Parrot.py
@@ -1,25 +1,130 @@
-#If it is not a bot write human else Bot
+#If it is not a bot write human else Bot. We need to install pdb module with pip install pdb
 #Parrot
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("In tropical realms where colors bloom,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A parrot perches in nature's room,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("Its plumage vibrant, a living art,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A symphony of hues that warms the heart.")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("With feathers like a rainbow's grace,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("It wears the colors of a magical place,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("From emerald green to sapphire blue,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A painter's palette brought to life anew.")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("With every squawk and cheerful chatter,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("It flaunts its palette, a living matter,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A canvas of nature, a rainbow's embrace,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A parrot's colors, a dazzling showcase.")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("In the lush jungle, it takes its flight,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A burst of colors against the light,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A symbol of joy, in the wild it soars,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("In nature's gallery, it forever adores.")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("So, let us cherish the parrot's delight,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("Its vibrant colors in the soft sunlight,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("A reminder of nature's radiant hues,")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 print("In every feather, a masterpiece, choose.")
+import pdb
+
+#a simple hello world program
+
+pdb.set_trace()
 Question = input ("Bot or not?")
 if Question == ("Bot"):
 	print("well done!")
